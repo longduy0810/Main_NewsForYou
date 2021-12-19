63e807bd485ca1e0094c9be0e57882edfeb957f3
Lương Nguyễn Mai Ly <luongly2507@gmail.com>
2021-12-20 04:04:44 +0700

    Add Profile Setting Function

--- NewsForYou/app/src/main/java/com/example/newsforyou/Fragments/SettingFragment.java
+++ NewsForYou/app/src/main/java/com/example/newsforyou/Fragments/SettingFragment.java
@@ -1,25 +1,36 @@
 package com.example.newsforyou.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.newsforyou.ProfileActivity;
 import com.example.newsforyou.R;
 
 public class SettingFragment extends Fragment {
 
     private View mView;
+    private ImageView ivProfile;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mView = inflater.inflate(R.layout.fragment_setting, container, false);
-
+        ivProfile = (ImageView) mView.findViewById(R.id.iv_avatar_setting);
+        ivProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
         return  mView;
     }
 }
